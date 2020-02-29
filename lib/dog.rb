921239e5034b53e8a542f5032285ed24c0446407
Kirsten Keels <keelsk@gmail.com>
2020-02-29 19:04:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,20 @@ class Dog
   
   def initialize(name)
     @name = name
-    
-    @all << self
+    save
   end
   
   def self.all
     @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
   end
 end
